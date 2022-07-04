b50fc67387af454aa251eff6e81c04038e0dac98
Higashiguchi <yosei4071@gmail.com>
2022-07-05 00:14:09 +0900

    ○○.index.erbを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   
-  resources :youtube 
-  # get 'youtube/main'=>'youtube#main'
-  # root 'youtube#main'
+  get 'youtube/main'=>'youtube#main'
+  root 'youtube#main'
   
   get 'aboutthispage/index' => 'aboutthispage#index'
   
-  # get 'youtube/index' => 'youtube#index'
+  get 'youtube/index' => 'youtube#index'
   
-  # get 'youtube/new'  => 'youtube#new'
+  get 'youtube/new'  => 'youtube#new'
   
-  # post 'youtube/:id' => 'youtube#create'
+  post 'youtube/:id' => 'youtube#create'
   
   end
